7903884aa252683bbb59e9627aba961fae678b3e
hanirahman <hanirahman029@gmail.com>
2023-09-28 10:54:47 +0700

    penambahan isi contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Hanifa Rahmania 
+        saya tinggal di Tambun Selatan
+        olahraga yang saya sukai adalah renang";
+    }
+}
+?>
