1641aaa82a25d2cb087e17e06b611254eeadb616
hanirahman <hanirahman029@gmail.com>
2023-10-05 20:01:59 +0700

    penambahan isi view-latihan

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -10,7 +10,11 @@ class latihan1 extends CI_Controller
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('model_latihan1');
-        $hasil = $this->model_latihan1->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari". $n1 ."+". $n2 ."=".$hasil;
+        
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->model_latihan1->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan', $data);
     }
 }
