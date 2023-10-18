d990bf199a8b5f10fb33bbf531c9398454a06e14
hanirahman <hanirahman029@gmail.com>
2023-10-18 19:48:24 +0700

    penambahan isi file baru

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan      : Hanifa</li>
+        <li>Nama Belakang   : Rahmania</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Kp.Buwek RT 01 RW 022 Desa Sumber Jaya Kecamatan Tambun Selatan</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+
+    <h4>Olahraga Favorite</h4>
+    <ul type="square">
+        <li>Renang</li>
+        <li>Jogging</li>
+    </ul>
+</section>
