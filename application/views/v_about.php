74bd8767e34bcd4418be24ead25220b0becbf44e
hanirahman <hanirahman029@gmail.com>
2023-10-20 14:47:05 +0700

    penambahan isi file baru

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -1,26 +1,23 @@
 <section>
-    <h1><?php echo $judul ?></h1>
+    
+    <img src="<?php echo base_url() ?>assets/img/honey.jpg" width="300px" height="350px">
+    <div>
+        <h4>HANIFA RAHMANIA</h4>
 
-
-    <h4>Nama</h4>
-    <ul type="disc">
-        <li>Nama Depan      : Hanifa</li>
-        <li>Nama Belakang   : Rahmania</li>
-    </ul>
-    <br>
-    <h4>Alamat</h4>
-    <ul type="none">
-        <li>Kp.Buwek RT 01 RW 022 Desa Sumber Jaya Kecamatan Tambun Selatan</li>
-    </ul>
-
-    <h4>Tempat Lahir</h4>
-    <ul type="none">
-        <li>Bekasi</li>
-    </ul>
-
-    <h4>Olahraga Favorite</h4>
-    <ul type="square">
-        <li>Renang</li>
-        <li>Jogging</li>
-    </ul>
+        <h4>Alamat</h4>
+        <ul type="none">
+            <li>Kp.Buwek RT 01 RW 022 Desa Sumber Jaya Kecamatan Tambun Selatan</li>
+        </ul>
+    
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+            <li>Bekasi</li>
+        </ul>
+    
+        <h4>Olahraga Favorite</h4>
+        <ul type="square">
+            <li>Renang</li>
+            <li>Jogging</li>
+        </ul>
+    </div>
 </section>
